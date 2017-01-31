e71322949430da1066cd007ae9a22d0a8a5f7211
Ryan Chenkie <ryanchenkie@gmail.com>
2017-01-31 16:14:59 -0500

    support HS and RS

--- 01-Authenticate-HS256/server.js
+++ 01-Authenticate-HS256/server.js
@@ -1,38 +1,26 @@
 const express = require('express');
 const app = express();
 const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 require('dotenv').config();
 
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_CLIENT_ID || !process.env.AUTH0_CLIENT_SECRET) {
+  throw 'Make sure you have AUTH0_DOMAIN, AUTH0_CLIENT_ID, and AUTH0_CLIENT_SECRET in your .env file'
 }
 
 app.use(cors());
 
 // Authentication middleware. When used, the
 // access token must exist and be verified against
-// the Auth0 JSON Web Key Set
+// the signing secret for the API
 const authenticate = jwt({
   // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
-
+  secret: process.env.AUTH0_CLIENT_SECRET,
   // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
+  audience: process.env.AUTH0_CLIENT_ID,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
+  algorithms: ['HS256']
 });
-
-// Authorization middleware. When used, the
-// access token must contain a scope of read:messages
-const authorize = jwtAuthz([ 'read:messages' ]);
 
 app.get('/api/public', function(req, res) {
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
@@ -40,10 +28,6 @@ app.get('/api/public', function(req, res) {
 
 app.get('/api/private', authenticate, function(req, res) {
   res.json({ message: "Hello from a private endpoint! You DO need to be authenticated to see this." });
-});
-
-app.get('/api/private/admin', authenticate, authorize, function(req, res) {
-  res.json({ message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." });
 });
 
 app.listen(3001);
--- 02-Authenticate-RS256/server.js
+++ 02-Authenticate-RS256/server.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const app = express();
 const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 require('dotenv').config();
 
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE || !process.env.AUTH0_HS256_SECRET) {
-  throw 'Make sure you have AUTH0_DOMAIN, AUTH0_AUDIENCE, and AUTH0_HS256_SECRET in your .env file'
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_CLIENT_ID) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_CLIENT_ID in your .env file'
 }
 
 app.use(cors());
 
 // Authentication middleware. When used, the
 // access token must exist and be verified against
-// the signing secret for the API
+// the Auth0 JSON Web Key Set
 const authenticate = jwt({
   // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: process.env.AUTH0_HS256_SECRET,
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['HS256']
-});
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
 
-// Authorization middleware. When used, the
-// access token must contain a scope of read:messages
-const authorize = jwtAuthz([ 'read:messages' ]);
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_CLIENT_ID,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
 
 app.get('/api/public', function(req, res) {
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
@@ -33,10 +35,6 @@ app.get('/api/public', function(req, res) {
 
 app.get('/api/private', authenticate, function(req, res) {
   res.json({ message: "Hello from a private endpoint! You DO need to be authenticated to see this." });
-});
-
-app.get('/api/private/admin', authenticate, authorize, function(req, res) {
-  res.json({ message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." });
 });
 
 app.listen(3001);
