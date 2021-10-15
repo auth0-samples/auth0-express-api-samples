1f1f7e36b2bb24d53825841c2fb1e5aec6006b9c
adamjmcgrath <adam.mcgrath@auth0.com>
2021-10-15 16:25:07 +0100

    Update the samples to use express-oauth2-jwt-bearer

--- 02-Authorization-HS256/server.js
+++ 02-Authorization-HS256/server.js
@@ -1,37 +1,26 @@
 const express = require('express');
 const app = express();
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 const cors = require('cors');
 require('dotenv').config();
 
-if (
-  !process.env.AUTH0_DOMAIN ||
-  !process.env.AUTH0_AUDIENCE ||
-  !process.env.AUTH0_API_SECRET
-) {
-  throw 'Make sure you have AUTH0_DOMAIN, AUTH0_AUDIENCE, and AUTH0_API_SECRET in your .env file';
+if (!process.env.ISSUER || !process.env.AUDIENCE || !process.env.SECRET) {
+  throw 'Make sure you have ISSUER, AUDIENCE and SECRET in your .env file';
 }
 
-app.use(cors());
+const corsOptions =  {
+  origin: 'http://localhost:3000'
+};
 
-// Authentication middleware. When used, the
-// access token must exist and be verified against
-// the signing secret for the API
-const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: process.env.AUTH0_API_SECRET,
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['HS256']
+app.use(cors(corsOptions));
+
+const checkJwt = auth({
+  tokenSigningAlg: 'HS256'
 });
-
-const checkScopes = jwtAuthz(['read:messages']);
 
 app.get('/api/public', function(req, res) {
   res.json({
-    message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
   });
 });
 
@@ -41,7 +30,7 @@ app.get('/api/private', checkJwt, function(req, res) {
   });
 });
 
-app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+app.get('/api/private-scoped', checkJwt, requiredScopes('read:messages'), function(req, res) {
   res.json({
     message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
   });
@@ -49,7 +38,7 @@ app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  return res.status(err.status).json({ message: err.message });
+  return res.set(err.headers).status(err.status).json({ message: err.message });
 });
 
 app.listen(3010);
