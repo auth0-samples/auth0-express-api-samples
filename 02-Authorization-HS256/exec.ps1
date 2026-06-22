e86c75e7c2a2449d2a5e8f0097880a32381c557e
Frederik Prijck <frederik.prijck@okta.com>
2026-06-22 18:20:57 +0200

    Remove HS256 example as auth0-express-api does not support HS256

--- 02-Authorization-HS256/exec.ps1
+++
@@ -1,2 +0,0 @@
-docker build -t auth0-express-api-rs256 .
-docker run --env-file .env -p 3010:3010 -it auth0-express-api-rs256
--- 02-Authorization-HS256/exec.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker build -t auth0-express-api-hs256 .
-docker run --env-file .env -p 3010:3010 -it auth0-express-api-hs256
